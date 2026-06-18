c48d2d2fe92af6f4cc2b61b40801919f3517d1b4
Herdy Cen <32483261+herdyc@users.noreply.github.com>
2026-06-18 08:57:33 +0700

    Add state objects for app state and authentication

--- ios/FitRewardApp/FitRewardApp/App/FitRewardApp.swift
+++ ios/FitRewardApp/FitRewardApp/App/FitRewardApp.swift
@@ -2,9 +2,14 @@ import SwiftUI
 
 @main
 struct FitRewardApp: App {
+    @StateObject private var appState = AppState()
+    @StateObject private var supabaseAuth = SupabaseAuth()
+
     var body: some Scene {
         WindowGroup {
             MainTabView()
+                .environmentObject(appState)
+                .environmentObject(supabaseAuth)
         }
     }
 }
