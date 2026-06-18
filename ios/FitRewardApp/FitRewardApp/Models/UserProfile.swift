f7195530fa0ea33da4086636acd4ac45dbdeb517
Herdy Cen <32483261+herdyc@users.noreply.github.com>
2026-06-18 08:56:54 +0700

    Add computed property for user initials

--- ios/FitRewardApp/FitRewardApp/Models/UserProfile.swift
+++ ios/FitRewardApp/FitRewardApp/Models/UserProfile.swift
@@ -7,4 +7,10 @@ struct UserProfile: Codable {
     var goal: String
     var totalPoints: Int
     var currentStreak: Int
+
+    var initials: String {
+        let parts = name.split(separator: " ")
+        let letters = parts.prefix(2).compactMap { $0.first }
+        return letters.map(String.init).joined().uppercased()
+    }
 }
